6f52660ff4a2bd5c2b64b9393c4cc3d1715de797
ItaloRez <italo.rezende@sharkdev.com.br>
2023-03-25 15:34:21 -0300

    aula 1

---
+++ workspace_eclipse/lab_jpa/lab_jpa/src/main/java/br/inatel/labs/labjpa/entity/NotaCompraItem.java
@@ -0,0 +1,20 @@
+package br.inatel.labs.labjpa.entity;
+
+import java.math.BigDecimal;
+
+public class NotaCompraItem {
+	
+	private Long id;
+	
+	private NotaCompra notaCompra;
+	
+	private Produto produto;
+	
+	private BigDecimal valorCompraProduto;
+	
+	private Integer quantidade;
+
+	public BigDecimal getCalculoTotalItem() {
+		return valorCompraProduto.multiply(BigDecimal.valueOf(quantidade));
+	}
+}
